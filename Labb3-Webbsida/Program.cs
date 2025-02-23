4b651dabee6a7e12f678929e9357f392864e2992
Johan Hultin <johan.hultin99@gmail.com>
2025-02-23 18:41:14 +0100

    git api added

--- Labb3-Webbsida/Program.cs
+++ Labb3-Webbsida/Program.cs
@@ -50,7 +50,7 @@ namespace Labb3_Webbsida
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            } 
 
             app.UseHttpsRedirection();
 
